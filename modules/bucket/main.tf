8cc1a3a67c0af552a9589100320181e71d3b6f60
goelanuj <goelanuj_us@yahoo.com>
2023-01-27 17:51:06 -0600

    u

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "auto-expire" {
   prefix = "${var.env}"
-  name = "ag_pictures"
+  name = "${var.env}-ag_pictures"
   location = "us"
   storage_class = "STANDARD"
 }
