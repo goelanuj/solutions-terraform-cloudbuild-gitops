74b0b99102f40341cf4c93deeed1e32a6f3edd9b
goelanuj <goelanuj_us@yahoo.com>
2023-01-30 17:20:37 -0600

    fixed ubuntu

--- modules/ubuntu_vm/main.tf
+++ modules/ubuntu_vm/main.tf
@@ -1,9 +1,14 @@
 # Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
+locals {
+  network = "${element(split("-", var.subnet), 0)}"
+}
+
+resource "google_compute_instance" "ubuntu_vm" {
+  project      = "${var.project}"
   name         = "${local.network}-ubuntu-instance"
   machine_type = "f1-micro"
   zone         = "us-west1-a"
-  tags         = ["ssh"]
+  tags         = ["ubuntu-vm"]
 
   boot_disk {
     initialize_params {
@@ -15,11 +20,12 @@ resource "google_compute_instance" "default" {
   # metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
 
   network_interface {
-    subnetwork = ${var.env}-subnet-01
-
-
+    subnetwork = "${var.subnet}"
 
     access_config {
       # Include this section to give the VM an external IP address
     }
   }
+# Apply the firewall rule to allow external IPs to access this instance
+  tags = ["http-server"]
+}
