7335ff2789a0adc59859eeda7b42e7f10a4f8f0b
goelanuj <goelanuj_us@yahoo.com>
2023-01-30 17:10:36 -0600

    added ubuntu

---
+++ modules/ubuntu_vm/main.tf
@@ -0,0 +1,25 @@
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "${local.network}-ubuntu-instance"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  # metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = ${var.env}-subnet-01
+
+
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
